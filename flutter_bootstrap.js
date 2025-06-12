758987d9c36f07bbf670e442662bc289e5740cf9
maximu211 <maximu211@users.noreply.github.com>
2025-06-12 14:31:08 +0000

    deploy: 90e045f1da8c6234cd1d10ec4666dd7d978bb92d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3808340129"
+    serviceWorkerVersion: "776052452"
   }
 });
